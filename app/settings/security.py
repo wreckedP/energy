7ec362ec8ec994cd9a91a99601d45ea8cb684fc6
stijn <stijnvdpouw@gmail.com>
2024-08-20 21:36:08 +0200

    extract token encryption

--- app/settings/security.py
+++ app/settings/security.py
@@ -17,23 +17,27 @@ def create_hash(password: str) -> str:
 def verify_hash(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
+def encrypt(data: dict[str, Any]):
+    return encode(data, configuration.private_key, algorithm="HS256")
+
+def decrypt(token: str):
+    return decode(token, configuration.private_key, algorithms="HS256")
 
 def encode_token(subject: Union[str, Any], expires_delta: timedelta) -> str:
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
         expire = datetime.utcnow() + timedelta(minutes=float(configuration.token_expire_minutes))
-    to_encode: dict[str, Any] = {"exp": expire, "sub": str(subject)}
-    encoded_jwt = encode(to_encode, configuration.private_key, algorithm="HS256")
 
-    return encoded_jwt
+    return encrypt({
+        "exp": expire, 
+        "sub": str(subject)
+        })
 
 
 def decode_token(token: str) -> TokenPayload:
     try:
-        payload = decode(token, configuration.private_key, algorithms="HS256")
-
-        return TokenPayload(**payload)
+        return TokenPayload(**decrypt(token))
 
     except JWTError as error:
         raise HTTPException(403, f"Invalid token: {error}")
