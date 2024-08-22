4a82383471fe9d2d19e68cc906db9fb6b7f2637e
WerckedP <stijnvdpouw@gmail.com>
2024-08-22 20:28:55 +0200

    ...

--- app/database/session.py
+++ app/database/session.py
@@ -1,14 +1,5 @@
-from asyncio import current_task
-
 from sqlalchemy import create_engine
-from sqlalchemy.ext.asyncio import (
-    create_async_engine,
-    AsyncSession,
-    AsyncScalarResult,
-    async_sessionmaker,
-    async_scoped_session,
-)
-from sqlalchemy.orm import Session, declarative_base, sessionmaker, scoped_session
+from sqlalchemy.orm import Session, declarative_base, sessionmaker
 
 
 from app.settings.configuration import configuration
@@ -17,12 +8,6 @@ Base = declarative_base()
 
 session_factory = sessionmaker(autoflush=False, bind=create_engine(configuration.db_driver + configuration.db_url + configuration.db_name))
 session = session_factory()
-
-async_engine = create_async_engine(
-    configuration.db_async_driver + configuration.db_url + configuration.db_name,
-    echo=True,
-)
-async_session_factory = async_sessionmaker(async_engine, expire_on_commit=False)
 
 
 def pg_session():
@@ -34,13 +19,3 @@ def pg_session():
     finally:
         session.commit()
         session.close()
-
-
-async def async_pg_session():
-    session = async_sessionmaker(async_engine, expire_on_commit=False)
-    try:
-        return session()
-    except Exception as err:
-        raise err
-    finally:
-        await async_engine.dispose()
